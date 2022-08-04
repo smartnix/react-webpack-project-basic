3dfb3518112d74bd7241bc686d3bea711ca0727c
smartnix <smartnix@hotmail.com>
2022-08-04 16:19:02 -0500

    Maquetacion y estilos del home

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/components/Footer.css';
+
+const Footer = () => {
+  return (
+    <div className='Footer'>
+        <p className="Footer-title">COnf MErch</p>
+        <p className="Footer-copy">
+            Footer
+        </p>
+    </div>
+  )
+}
+
+export {Footer}
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/components/Header.css';
+
+
+const Header = () => {
+  return (
+    <div className='Header'>
+        <h1 className="Header-title">Merch</h1>
+        <div className="Header-checkout">
+            Checkout
+        </div>
+    </div>
+  )
+}
+
+export {Header}
