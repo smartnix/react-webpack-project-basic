ff0731d3a936cbb1bca89691b226ff80db6cd264
smartnix <smartnix@hotmail.com>
2022-08-03 22:04:48 -0500

    Proyecto configuracion basica react-webpack

---
+++ src/routes/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div><h1>APP</h1></div>
+  )
+}
+
+export  {App}
